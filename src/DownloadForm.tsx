b13c8457077d8d9217bb9c961714522e2b7140d4
c-rainbow <46095062+c-rainbow@users.noreply.github.com>
2022-07-07 00:13:57 -0700

    Radio button box and download button are centered

--- src/DownloadForm.tsx
+++ src/DownloadForm.tsx
@@ -6,34 +6,27 @@ export default function DownloadForm() {
   };
 
   return (
-    <form className="download-form" onSubmit={handleSubmit}>
-      <div className="m-3"></div>
-      <div className="m-3">
-        <div className="form-control">
-          <label className="label cursor-pointer">
-            <span className="label-text">Text</span>
-            <input
-              type="radio"
-              name="radio-6"
-              className="radio checked:bg-red-500"
-              checked
-            />
-          </label>
-        </div>
-        <div className="form-control">
-          <label className="label cursor-pointer">
-            <span className="label-text">JSON</span>
-            <input
-              type="radio"
-              name="radio-6"
-              className="radio checked:bg-blue-500"
-              checked
-            />
-          </label>
-        </div>
-      </div>
-      <div className="m-3">
-        <input type="submit" className="btn btn-primary" value="Download" />
+    <form className="download-form form-control flex items-center" onSubmit={handleSubmit}>
+      <div className="w-60">
+        <label className="label cursor-pointer">
+          <span className="label-text">Text</span>
+          <input
+            type="radio"
+            name="radio-6"
+            className="radio checked:bg-red-500"
+            checked
+          />
+        </label>
+        <label className="label cursor-pointer">
+          <span className="label-text">JSON</span>
+          <input
+            type="radio"
+            name="radio-6"
+            className="radio checked:bg-blue-500"
+            checked
+          />
+        </label>
+        <input type="submit" className="btn btn-primary mt-3" value="Download" />
       </div>
     </form>
   );
