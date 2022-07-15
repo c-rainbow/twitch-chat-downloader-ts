c2db973061dbe0d49c9aadef46aa4ae36459a9a3
c-rainbow <46095062+c-rainbow@users.noreply.github.com>
2022-07-14 20:16:37 -0700

    Implemented stop functionality

--- src/DownloadForm.tsx
+++ src/DownloadForm.tsx
@@ -1,39 +1,61 @@
-import { FormEvent } from 'react';
+import { FormEvent, useState } from 'react';
 import { TWITCH_CLIENT_ID } from './lib/constants';
 import { ChatDownloader } from './lib/downloader';
 import { useStore } from './lib/states';
+import { DownloadStatus } from './lib/types';
+
+
+enum FileType {
+  TEXT = 'text',
+  JSON = 'json',
+}
+
+
+const saveToFile = (output: string, videoId: string) => {
+  const blob = new Blob([output], {type: "application/json"});
+  const blobUrl = URL.createObjectURL(blob);
+  const a = document.createElement("a");
+  a.href = blobUrl;
+  a.download = `chats_${videoId}.json`;
+
+  const clickHandler = () => {
+      setTimeout(() => {
+          URL.revokeObjectURL(blobUrl);
+          //this.removeEventListener('click', clickHandler);
+      }, 150);
+  };
+
+  a.addEventListener('click', clickHandler, false);
+  a.click();
+};
 
 
 export default function DownloadForm() {
-  const [videoInfo, setLastChatOffset, startDownload, endDownload] = useStore(
+  const [videoInfo, setLastChatOffset, startDownload, endDownload, downloadStatus] = useStore(
     (state) => [
       state.videoInfo,
       state.setLastChatOffset,
       state.startDownload,
       state.endDownload,
+      state.downloadStatus,
     ]
   );
 
-  const saveToFile = (output: string, videoId: string) => {
-    const blob = new Blob([output], {type: "application/json"});
-    const blobUrl = URL.createObjectURL(blob);
-    const a = document.createElement("a");
-    a.href = blobUrl;
-    a.download = `chats_${videoId}.json`;
+  const [fileFormat, setFileFormat] = useState<FileType>(FileType.TEXT);
+  const [stopped, setStopped] = useState<boolean>(false);
 
-    const clickHandler = () => {
-        setTimeout(() => {
-            URL.revokeObjectURL(blobUrl);
-            //this.removeEventListener('click', clickHandler);
-        }, 150);
-    };
+  const handleFormatChange = (e: any) => {
+    console.log('value:', e.target.value);
+    setFileFormat(e.target.value);
+  }
 
-    a.addEventListener('click', clickHandler, false);
-    a.click();
-  };
+  const cancelDownload = () => {
+    setStopped(true);
+  }
 
   const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    console.log('File format:', fileFormat);
     if (!videoInfo) {
       console.log('No VOD to download');
       return;
@@ -46,7 +68,7 @@ export default function DownloadForm() {
     const chats = [];
     let ended = false;
     startDownload();
-    while (!ended) {
+    while (!ended && !stopped) {
       const batchResult = await downloader.downloadNextBatch();
       console.log('offset', batchResult.lastChatOffset);
       chats.push(...batchResult.comments);
@@ -55,6 +77,10 @@ export default function DownloadForm() {
     }
     endDownload();
     console.log('Downloaded', chats.length, 'chats');
+
+    if (stopped) {
+      return;
+    }
 
     const output = {
       video: videoInfo,
@@ -72,28 +98,39 @@ export default function DownloadForm() {
     >
       <div className="w-60">
         <label className="label cursor-pointer">
-          <span className="label-text">Text</span>
-          <input
-            type="radio"
-            name="radio-6"
-            className="radio checked:bg-red-500"
-            checked
-          />
-        </label>
-        <label className="label cursor-pointer">
           <span className="label-text">JSON</span>
           <input
             type="radio"
-            name="radio-6"
+            name="file-format"
+            value={FileType.JSON}
             className="radio checked:bg-blue-500"
             checked
+            onChange={handleFormatChange}
           />
         </label>
-        <input
-          type="submit"
-          className="btn btn-primary mt-3"
-          value="Download"
-        />
+        <label className="label cursor-pointer">
+          <span className="label-text">Text</span>
+          <input
+            type="radio"
+            name="file-format"
+            value={FileType.TEXT}
+            className="radio checked:bg-red-500"
+            checked
+            onChange={handleFormatChange}
+          />
+        </label>
+        {downloadStatus !== DownloadStatus.DOWNLOADING && (
+          <input
+            type="submit"
+            className="btn btn-primary mt-3"
+            value="Download"
+          />
+        )}
+        {downloadStatus === DownloadStatus.DOWNLOADING && (
+          <button className="btn btn-primary mt-3" onClick={cancelDownload}>
+            Cancel
+          </button>
+        )}
       </div>
     </form>
   );
